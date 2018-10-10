c872f19206c4121618eebfcad0bd43bb2ac7abe3
Misery <723690032@qq.com>
2018-10-10 19:30:45 +0800

    更换PAC处理方式，新增自动重启privoxy

--- privoxy_mode.py
+++ privoxy_mode.py
@@ -8,9 +8,17 @@
 from time import sleep
 from requests import get
 from base64 import b64decode
-from os import path,popen,remove,getcwd
+from os import path,popen,remove
+from shutil import copy
+
+#直接获取gfwlist,无需转换 6小时更新一次
+def updateGfwList():
+    IEProxy('disable')
+    url = 'https://zfl9.github.io/gfwlist2privoxy/gfwlist.action'
+    open('./privoxy/pac.action','wb').write(get(url).content)
 
 
+'''
 # 更新gfwlist 调用即可将gfw拦截的域名更新至最新，写入privoxy文件夹下的gfwlist文件中
 def get_gfwlist():
     url_list = ['https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt',
@@ -35,8 +43,8 @@ def get_gfwlist():
         return 'Network Error'
 
 
-# 修改注册表，传入参数
-'''
+# 修改注册表，传入参数(此方法不可用)
+
 def set_proxy(enable, proxy_ip):
     path = "Software\Microsoft\Windows\CurrentVersion\Internet Settings"
     key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, path, 0, winreg.KEY_WRITE)
@@ -45,58 +53,54 @@ def set_proxy(enable, proxy_ip):
 '''
 
 
-#更新生成pac文件
-def switch_gfwlist():
-    g_lines = open('./privoxy/gfwlist','r').readlines()
-    g_all = 'var direct = "__DIRECT__";if (direct == "__DIR" + "ECT__") direct = "DIRECT;";var wall_proxy = function(){ return "__PROXY__"; };var wall_v6_proxy = function(){ return "__PROXY__"; };var nowall_proxy = function(){ return direct; };var ip_proxy = function(){ return nowall_proxy(); };var ipv6_proxy = function(){ return nowall_proxy(); };var rules = [\n'
-    for g in g_lines:
-        if g[0] != '!' and g != '\n' and g[0] != '[':
-            g = '\t\"'+g.replace('\n','\",')+'\n'
-            g_all += g
-    g_all = g_all + '];\n'+open('./privoxy/pacpart','r').read()
-    open('./pac','w').write(g_all)
 
 
-
-#修改代理模式，自动生成注册表  参考:https://www.jianshu.com/p/49c444d9a435
-def set_proxy(proxy_mode):
+#是否启用IE代理，地址为 127.0.0.1:1080 写入注册表。参考:https://www.jianshu.com/p/49c444d9a435
+def IEProxy(enable):
     settings = 'Windows Registry Editor Version 5.00\n[HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Connections]\n\"DefaultConnectionSettings\"=hex:'
-    proxySwitchValue = '46,00,00,00,d2,06,00,00,01,00,00,00,0e,00,00,00,31,32,37,2e,30,2e,30,2e,31,3a,31,30,38,30,'
+    proxySwitchValue = '46,00,00,00,d2,06,00,00,01,00,00,00,0e,00,00,00,31,32,37,2e,30,2e,30,2e,31,3a,31,30,38,31,'
     fileteLocalValue = 'bf,00,00,00,6c,6f,63,61,6c,68,6f,73,74,3b,31,32,37,2e,2a,3b,31,30,2e,2a,3b,31,37,32,2e,31,36,2e,2a,3b,31,37,32,2e,31,37,2e,2a,3b,31,37,32,2e,31,38,2e,2a,3b,31,37,32,2e,31,39,2e,2a,3b,31,37,32,2e,32,30,2e,2a,3b,31,37,32,2e,32,31,2e,2a,3b,31,37,32,2e,32,32,2e,2a,3b,31,37,32,2e,32,33,2e,2a,3b,31,37,32,2e,32,34,2e,2a,3b,31,37,32,2e,32,35,2e,2a,3b,31,37,32,2e,32,36,2e,2a,3b,31,37,32,2e,32,37,2e,2a,3b,31,37,32,2e,32,38,2e,2a,3b,31,37,32,2e,32,39,2e,2a,3b,31,37,32,2e,33,30,2e,2a,3b,31,37,32,2e,33,31,2e,2a,3b,31,37,32,2e,33,32,2e,2a,3b,31,39,32,2e,31,36,38,2e,2a,3b,3c,6c,6f,63,61,6c,3e,48,'
-    if proxy_mode == 'pac':
-        pac_path = getcwd()+'\\pac'
-        pacValue = '00,00,00,'
-        for p in pac_path:
-            pacValue += format(ord(p),'x')+','
-        proxySwitchValue = proxySwitchValue.replace('01','07')
-        reg_value = settings + proxySwitchValue + fileteLocalValue + pacValue
-    elif proxy_mode == 'all':
+    if enable == 'enable':
         proxySwitchValue = proxySwitchValue.replace('01','03')
         reg_value = settings+proxySwitchValue+fileteLocalValue
-    else:
-        reg_value = settings + proxySwitchValue
-
+    elif enable == 'disable':
+        reg_value = settings+proxySwitchValue+fileteLocalValue
     open('./tmp.reg','w').write(reg_value)
     popen('reg import ./tmp.reg')
     sleep(0.5)
     remove('./tmp.reg')
 
 
+
+
 # 直连模式
 def direct_mode():
-    set_proxy('direct')
+    IEProxy('disable')
 
 
 # PAC模式
 def pac_mode():
-    if path.exists('./pac'):
-        set_proxy('pac')
+    if path.exists('./privoxy/pac.action'):
+        f = open('./privoxy/config.txt', 'r').read()
+        f = f.replace('forward-socks5 / 127.0.0.1:1080 .','actionsfile pac.action')
+        open('./privoxy/config.txt', 'w').write(f)
+        IEProxy('enable')
+        popen('taskkill /F /IM privoxy.exe')
+        sleep(0.3)
+        popen('cd privoxy && privoxy.exe')
     else:
-        get_gfwlist()
-        switch_gfwlist()
+        updateGfwList()
         pac_mode()
 
 
 # 全局模式
 def all_mode():
-    set_proxy('all')
+    f = open('./privoxy/config.txt','r').read()
+    f = f.replace('actionsfile pac.action','forward-socks5 / 127.0.0.1:1080 .')
+    open('./privoxy/config.txt','w').write(f)
+    IEProxy('enable')
+    popen('taskkill /F /IM privoxy.exe')
+    sleep(0.3)
+    popen('cd privoxy && privoxy.exe')
+
+pac_mode()
